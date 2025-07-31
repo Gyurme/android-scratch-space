377e0c26d5f07041e2b2d8105d5ecb1960cf424a
Gyurme Dahdul <gyurme@gmail.com>
2025-07-31 21:39:04 +1200

    Updated ViewModel to support pulling in Amphibian info

--- Amphibians/app/src/main/java/com/gyurme/amphibians/ui/screens/AmphibiansViewModel.kt
+++ Amphibians/app/src/main/java/com/gyurme/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -6,15 +6,37 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import coil3.network.HttpException
 import com.gyurme.amphibians.AmphibiansApplication
 import com.gyurme.amphibians.data.AmphibiansRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
 class AmphibiansViewModel (private val amphibiansRepository: AmphibiansRepository): ViewModel() {
 
     var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
         private set
+
+    init {
+        getAmphibians()
+    }
+
+    fun getAmphibians() {
+        viewModelScope.launch {
+            amphibiansUiState = AmphibiansUiState.Loading
+            amphibiansUiState = try {
+                val result = amphibiansRepository.getAmphibians()
+                AmphibiansUiState.Success(result)
+            } catch (e: IOException) {
+                AmphibiansUiState.Error
+            } catch (e: HttpException) {
+                AmphibiansUiState.Error
+            }
+        }
+    }
 
     companion object {
         val Factory : ViewModelProvider.Factory = viewModelFactory {
