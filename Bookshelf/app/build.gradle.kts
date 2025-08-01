3e67da58b7ed7e28c5d16947e8d1a5f64ff529eb
Gyurme Dahdul <gyurme@gmail.com>
2025-08-01 16:29:10 +1200

    Bumped dependencies, updated AndroidManifest file

--- Bookshelf/app/build.gradle.kts
+++ Bookshelf/app/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
 }
 
 android {
@@ -33,9 +41,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
