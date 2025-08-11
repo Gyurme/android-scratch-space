05c272e940612141d2432f13adc50fdd5a1b81b3
Gyurme Dahdul <gyurme@gmail.com>
2025-08-11 12:50:09 +1200

    Added Room, set up entity and DAO

--- Inventory/app/build.gradle.kts
+++ Inventory/app/build.gradle.kts
@@ -75,6 +75,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.room:room-ktx:2.7.2")
+    implementation("androidx.room:room-compiler:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2}")
 
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
